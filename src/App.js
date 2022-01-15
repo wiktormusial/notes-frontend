4a3ac706f2a20929f3de4a31adbd3e7236fbc917
Wiktor Musial <wiktormusial@icloud.com>
2022-01-15 19:18:47 +0100

    Create Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import AuthLogin from './views/Auth/AuthLogin/AuthLogin'
 
 function App() {
   return (
-    <div>
-      Test
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<AuthLogin />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

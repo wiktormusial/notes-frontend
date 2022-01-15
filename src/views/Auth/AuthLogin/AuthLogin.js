bbf5b32b0a09dae640e1475eb48df1df5eea0765
Wiktor Musial <wiktormusial@icloud.com>
2022-01-15 19:32:10 +0100

    Create AuthLoginForm

--- src/views/Auth/AuthLogin/AuthLogin.js
+++ src/views/Auth/AuthLogin/AuthLogin.js
@@ -1,8 +1,33 @@
+import { Formik, Form, Field } from 'formik';
+import { AuthLoginValidationSchema } from './AuthLoginValidationSchema'
+
 export default function AuthLogin () {
+
+  function handleUserClick(values) {
+    console.log(values)
+  }
 
   return(
     <div>
-      Test render
+      <h2>Login</h2>
+      <Formik
+        initialValues={{
+          username: '',
+          password: '',
+        }}
+        validationSchema={AuthLoginValidationSchema}
+        onSubmit={(values) => handleUserClick(values)}
+      >
+      {({ errors, touched, isValidating }) => (
+        <Form>
+          <Field name="username" /><br/>
+          {errors.username && touched.username && <div>{errors.username}</div>}
+          <Field name="password"/><br/>
+          {errors.password && touched.password && <div>{errors.password}</div>}
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+      </Formik>
     </div>
   )
 }

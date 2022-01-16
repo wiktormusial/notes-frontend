7f7d62dc655fbcdf256ee2a733bf049ee89b4dac
Wiktor Musial <wiktormusial@icloud.com>
2022-01-16 17:05:07 +0100

    create userslice

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import usersReducers from '@store/users/usersSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    users: usersReducers,
+  },
 });
---
+++ src/store/users/usersSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+import getToken from '@utils/Auth/getToken'
+
+export const getUserData = createAsyncThunk(
+  'users/getUserData',
+  async() => {
+    const token = getToken()
+    if (token !== undefined) {
+      const response = await axios({
+        method: 'get',
+        url: `${process.env.REACT_APP_API_URL}users/getuserinfo/`,
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Token ${token}`
+        }
+      })
+      return response.data
+    }
+  }
+)
+
+const initialState = {
+  status: 'idle',
+  is_logged_in: false,
+  error: null,
+  data: {}
+}
+
+export const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    userLoggedOut(state) {
+      state.is_logged_in = false
+      state.data = []
+    }
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(getUserData.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.is_logged_in = true
+        state.data = action.payload
+      })
+      .addCase(getUserData.rejected, (state) => {
+        state.status = 'error'
+      })
+  },
+})
+
+export default usersSlice.reducer;

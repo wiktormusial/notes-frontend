184d0ebf66b16f46c24e3713a5968008a1c4d02f
Wiktor Musial <wiktormusial@icloud.com>
2022-01-16 19:02:37 +0100

    Create fetchNotes function

--- src/store/notes/notesSlice.js
+++ src/store/notes/notesSlice.js
@@ -2,6 +2,24 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 import getToken from '@utils/Auth/getToken'
 
+export const fetchNotes = createAsyncThunk(
+  'notes/fetchNotes',
+  async() => {
+    const token = getToken()
+    if (token !== undefined) {
+      const response = await axios({
+        method: 'get',
+        url: `${process.env.REACT_APP_API_URL}notes`,
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Token ${token}`
+        }
+      })
+      return response.data
+    }
+  }
+)
+
 const initialState = {
   status: 'idle',
   error: null,

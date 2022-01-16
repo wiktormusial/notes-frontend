8f111e3ad9041069ffaefd870347df006a5c7079
Wiktor Musial <wiktormusial@icloud.com>
2022-01-16 17:48:19 +0100

    Do not display Register form when user is logged

--- src/views/Auth/AuthRegister/AuthRegister.js
+++ src/views/Auth/AuthRegister/AuthRegister.js
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Formik, Form, Field } from 'formik';
 import { useNavigate } from "react-router-dom";
-import { AuthRegisterValidationSchema } from './AuthRegisterValidationSchema'
+import { getUserData, getUserLogStatus } from '@store/users/usersSlice'
 import userRegister from '@utils/Auth/userRegister'
+import { UserLogged } from '@components/Errors/UserLogged'
+import { AuthRegisterValidationSchema } from './AuthRegisterValidationSchema'
 
 export default function AuthRegister () {
   const [registerStatus, setRegisterStatus] = useState()
   const [error, setError] = useState()
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const userLoginStatus = useSelector(getUserLogStatus)
 
   function handleUserClick(values) {
     userRegister(values)
@@ -17,12 +22,13 @@ export default function AuthRegister () {
   useEffect(() => {
     if (registerStatus !== undefined) {
       if (registerStatus.key) {
+        dispatch(getUserData())
         navigate('/register/success')
       } else {
         setError(registerStatus)
       }
     }
-  }, [registerStatus, navigate])
+  }, [registerStatus, navigate, dispatch])
 
   let error_list
 
@@ -31,39 +37,43 @@ export default function AuthRegister () {
       return <div key={i}>{i}</div>
     })
   }
-  
-  return(
-    <div>
-      <h2>Register</h2>
-      <Formik
-        initialValues={{
-          username: '',
-          password: '',
-          password2: '',
-          email: '',
-        }}
-        validationSchema={AuthRegisterValidationSchema}
-        onSubmit={(values) => handleUserClick(values)}
-      >
-      {({ errors, touched, isValidating }) => (
-        <Form>
-          <label htmlFor="username">Username</label><br/>
-          <Field name="username" /><br/>
-          {errors.username && touched.username && <div>{errors.username}</div>}
-          <label htmlFor="email">Email</label><br/>
-          <Field name="email" /><br/>
-          {errors.email && touched.email && <div>{errors.email}</div>}
-          <label htmlFor="password">Password</label><br/>
-          <Field name="password" type="password"/><br/>
-          {errors.password && touched.password && <div>{errors.password}</div>}
-          <label htmlFor="password2">Retype password</label><br/>
-          <Field name="password2" type="password"/><br/>
-          {errors.password2 && touched.password2 && <div>{errors.password2}</div>}
-          {error_list}
-          <button type="submit">Submit</button>
-        </Form>
-      )}
-      </Formik>
-    </div>
-  )
+
+  if (userLoginStatus === true) {
+    return <UserLogged />
+  } else {
+    return(
+      <div>
+        <h2>Register</h2>
+        <Formik
+          initialValues={{
+            username: '',
+            password: '',
+            password2: '',
+            email: '',
+          }}
+          validationSchema={AuthRegisterValidationSchema}
+          onSubmit={(values) => handleUserClick(values)}
+        >
+        {({ errors, touched, isValidating }) => (
+          <Form>
+            <label htmlFor="username">Username</label><br/>
+            <Field name="username" /><br/>
+            {errors.username && touched.username && <div>{errors.username}</div>}
+            <label htmlFor="email">Email</label><br/>
+            <Field name="email" /><br/>
+            {errors.email && touched.email && <div>{errors.email}</div>}
+            <label htmlFor="password">Password</label><br/>
+            <Field name="password" type="password"/><br/>
+            {errors.password && touched.password && <div>{errors.password}</div>}
+            <label htmlFor="password2">Retype password</label><br/>
+            <Field name="password2" type="password"/><br/>
+            {errors.password2 && touched.password2 && <div>{errors.password2}</div>}
+            {error_list}
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+        </Formik>
+      </div>
+    )
+  }
 }

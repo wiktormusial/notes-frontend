7ce6ef6198b9b130c1c2a41e60f650690649c347
Wiktor Musial <wiktormusial@icloud.com>
2022-01-16 17:36:30 +0100

    Dispatch getUserInfo in header

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,4 +1,17 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getUserData, getUsersStatus } from '@store/users/usersSlice'
+
 export default function Navbar () {
+  const dispatch = useDispatch()
+  const getUserStatus = useSelector(getUsersStatus)
+
+  useEffect(() => {
+    if (getUserStatus === 'idle') {
+      dispatch(getUserData())
+    }
+  })
+
   return(
     <h1>Navbar</h1>
   )

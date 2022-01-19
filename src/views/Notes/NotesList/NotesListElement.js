9b85fdf00ae2a6505dace582d478a3fb67fe3602
Wiktor Musial <wiktormusial@icloud.com>
2022-01-19 17:01:24 +0100

    Use dateFormatter function

--- src/views/Notes/NotesList/NotesListElement.js
+++ src/views/Notes/NotesList/NotesListElement.js
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom'
+import { dateFormatter } from '@utils/Date/dateFormatter'
 
 export default function NotesListElement(props) {
-  const { id, slug, title, created_at, body, category } = props.note
+  const { slug, title, created_at, body, category } = props.note
 
   return (
     <div>
-      <Link to={`${slug}`}><h3>{title}</h3></Link>
-      <p>{created_at}</p>
+      <Link to={`/${slug}`}><h3>{title}</h3></Link>
+      <p>{dateFormatter(created_at)}</p>
       <p>{body}</p>
       <p>{category}</p>
     </div>
